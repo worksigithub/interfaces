14fd2bf063cc73f372750e96287a2a483ceabab6
worksigithub <rogworksi@gmail.com>
2020-06-01 14:52:17 -0300

    Juego 4 en linea Fix control partida iniciada

--- TP2/js/js.js
+++ TP2/js/js.js
@@ -4,6 +4,16 @@ let contenedor = new Contenedor("#contenedor");
 contenedor.drawInicio();
 
 document.querySelector("#btnComenzar").addEventListener('click', r => {
+    if (contenedor.jugando) {
+        $("#modalInicio").modal("hide");
+        $("#modalControl").modal("show");
+
+    } else {
+        contenedor.comenzarJuego();
+    }
+});
+
+document.querySelector("#btnIniciar").addEventListener('click', r => {
+    $("#modalControl").modal("hide");
     contenedor.comenzarJuego();
-    //contenedor.agregarFigura('circulo');
 });
